717701b2b505409fddd31db4f70662a01e0d7ad7
Wallace <dev_kollen@outlook.com>
2024-03-13 17:06:49 -0300

    feat: :sparkles: First commit and config with graphql

---
+++ codegen.ts
@@ -0,0 +1,15 @@
+import { CodegenConfig } from "@graphql-codegen/cli"
+
+const config: CodegenConfig = {
+  schema: "http://localhost:3000/graphql",
+  documents: ["src/graphql/**/*.ts"],
+  ignoreNoDocuments: true,
+  generates: {
+    "./src/gql/": {
+      preset: "client",
+      plugins: ["typescript"],
+    },
+  },
+}
+
+export default config
